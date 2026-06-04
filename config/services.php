bf92ce44e2c3e448d7af13bb2a09e79f8f8e8fa5
Herman <hermantaufiq12@gmail.com>
2026-06-04 22:51:08 +0700

    fix: add xendit configuration block to services.php

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'xendit' => [
+        'secret_key'    => env('XENDIT_SECRET_KEY', ''),
+        'webhook_token' => env('XENDIT_WEBHOOK_TOKEN', ''),
+        'base_url'      => env('XENDIT_BASE_URL', 'https://api.xendit.co'),
+    ],
+
 ];
